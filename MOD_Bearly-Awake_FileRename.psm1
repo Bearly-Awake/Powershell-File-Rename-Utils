600e12cff940a6e03ed957068d75e3a10da37b1a
Bearly-Awake <47607334+Bearly-Awake@users.noreply.github.com>
2019-03-03 19:43:50 +0100

    Update MOD_Bearly-Awake_FileRename.psm1

--- MOD_Bearly-Awake_FileRename.psm1
+++ MOD_Bearly-Awake_FileRename.psm1
@@ -1,15 +1,32 @@
+#
+#This is a template for future functions
+#function rename-Item____($path){
+#    $files = Get-ChildItem $path | select *
+#    $currentpath  = Get-Location
+#    cd $path
+#        foreach ($file in $files){
+#            Rename-Item  -LiteralPath $file.FullName -NewName $($file.name -replace "","")
+#        }
+#    cd $currentpath 
+#}
+
+
 function rename-ItemRemoveSquareBrackets($path){
-$files = Get-ChildItem $path | select *
-cd $path
-    foreach ($file in $files){
-        Rename-Item  -LiteralPath $file.FullName -NewName $($file.name -replace "\[(.*?)\]","")
-        }
+    $files = Get-ChildItem $path | select *
+    $files = Get-ChildItem $path | select *
+    $currentpath  = Get-Location
+        foreach ($file in $files){
+            Rename-Item  -LiteralPath $file.FullName -NewName $($file.name -replace "\[(.*?)\]","")
+            }
+    cd $currentpath  = Get-Location
 }
 
 function rename-ItemRemovePeriodReplaceWithSpace($path){
-$files = Get-ChildItem $path | select *
-cd $path
-    foreach ($file in $files){
-        Rename-Item  -LiteralPath $file.FullName -NewName $($file.name -replace "\.(?=.*\.)"," ")
-    }
+    $files = Get-ChildItem $path | select *
+    $currentpath  = Get-Location
+    cd $path
+        foreach ($file in $files){
+            Rename-Item  -LiteralPath $file.FullName -NewName $($file.name -replace "\.(?=.*\.)"," ")
+        }
+    cd $currentpath  = Get-Location
 }
